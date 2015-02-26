88d679730380365d681b55648a572080a7f12896
Mairead Buchan <mairead.buchan@headlondon.com>
2015-02-26 16:29:28 +0000

    Adding new article pages

--- app.js
+++ app.js
@@ -37,11 +37,34 @@ app.get('/slides', function(req, res){
 });
 
 app.get('/articles', articles.year15);
-app.get('/articles/2011', articles.year11);
+app.get('/articles/2011/', articles.year11);
 app.get('/articles/2012', articles.year12);
 app.get('/articles/2013', articles.year13);
 app.get('/articles/2014', articles.year14);
 app.get('/articles/2015', articles.year15);
+
+app.get('/articles/2011/creating-responsive-images-using-the-noscript-tag', articles.article01);
+app.get('/articles/2011/the-accuracy-of-javascript-timing', articles.article02);
+app.get('/articles/2011/net-magazine-asks-how-we-created-our-site', articles.article03);
+app.get('/articles/2011/vive-la-revolution-bitcoin-the-currency-of-the-internet', articles.article04);
+app.get('/articles/2011/full-frontal-conference-2011', articles.article05);
+
+app.get('/articles/2012/the-incredible-and-amazing-arduino-vote-o-matic', articles.article06);
+app.get('/articles/2012/creating-a-responsive-layout-website-part-1', articles.article07);
+app.get('/articles/2012/creating-a-responsive-layout-website-part-2', articles.article08);
+app.get('/articles/2012/the-joy-of-sass-or-how-i-learned-to-stop-worrying-and-love-the-preprocessor', articles.article09);
+app.get('/articles/2012/full-frontal-2012', articles.article10);
+
+app.get('/articles/2013/microsoft-kinect-and-the-web-browser-how-to-make-it-happen', articles.article11);
+app.get('/articles/2013/microsoft-kinect-and-the-web-browser-enter-zigfu', articles.article12);
+app.get('/articles/2013/kinect-pain-reinstalling-libfreenect-kinect-drivers-on-mountain-lion-upgrade', articles.article13);
+
+app.get('/articles/2014/kings-cultural-institute-culture-hack', articles.article14);
+app.get('/articles/2014/why-i-quit-my-job', articles.article15);
+app.get('/articles/2014/setting-up-a-limited-company', articles.article16);
+
+app.get('/articles/2015/creating-a-frontend-workflow-with-gulp-and-browserify', articles.article17);
+app.get('/articles/2015/getting-started-with-es6', articles.article18);
 
 app.get('/golden_path', golden_path.index)
 app.get('/golden_path_manifesto', golden_path.manifesto);
--- routes/articles.js
+++ routes/articles.js
@@ -16,3 +16,58 @@ exports.year14 = function(req, res){
 exports.year15 = function(req, res){
   res.render('year15', { title: 'Articles: 2015' });
 };
+
+exports.index = function(req, res){
+  res.render('article01', { title: 'Articles: Creating responsive images using the noscript tag' });
+};
+exports.index = function(req, res){
+  res.render('article02', { title: 'Articles: The accuracy of Javascript timing' });
+};
+exports.index = function(req, res){
+  res.render('article03', { title: 'Articles: .Net Magazine asks how we created our site' });
+};
+exports.index = function(req, res){
+  res.render('article04', { title: 'Articles: Vive la revolution: Bitcoin (the currency of the internet)' });
+};
+exports.index = function(req, res){
+  res.render('article05', { title: 'Articles: Full Frontal conference 2011 - UKs Best Front End Developers Conference' });
+};
+exports.index = function(req, res){
+  res.render('article06', { title: 'Articles: The incredible and amazing Arduino Vote-O-Matic' });
+};
+exports.index = function(req, res){
+  res.render('article07', { title: 'Articles: Creating a responsive layout website: Part I - Using media queries' });
+};
+exports.index = function(req, res){
+  res.render('article08', { title: 'Articles: Creating a responsive layout website: Part II - Making images work' });
+};
+exports.index = function(req, res){
+  res.render('article09', { title: 'Articles: The joy of Sass or "how I learned to stop worrying and love the preprocessor"' });
+};
+exports.index = function(req, res){
+  res.render('article10', { title: 'Articles: Full Frontal 2012' });
+};
+exports.index = function(req, res){
+  res.render('article11', { title: 'Articles: Microsoft Kinect and the web browser - How to make it happen' });
+};
+exports.index = function(req, res){
+  res.render('article12', { title: 'Articles: Microsoft Kinect and the web browser - enter Zigfu' });
+};
+exports.index = function(req, res){
+  res.render('article13', { title: 'Articles: Kinect pain - Reinstalling Libfreenect Kinect drivers on Mountain Lion upgrade' });
+};
+exports.index = function(req, res){
+  res.render('article14', { title: 'Articles: Kings Cultural Institute - Culture Hack' });
+};
+exports.index = function(req, res){
+  res.render('article15', { title: 'Articles: Why I quit my job' });
+};
+exports.index = function(req, res){
+  res.render('article16', { title: 'Articles: Setting up a limited company' });
+};
+exports.index = function(req, res){
+  res.render('article17', { title: 'Articles: Creating a frontend workflow with Gulp and Browserify' });
+};
+exports.index = function(req, res){
+  res.render('article18', { title: 'Articles: Getting started with ES6' });
+};
